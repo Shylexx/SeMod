a3a5e77547ed81af1cc4f23279d52372649a9eb8
Shylexx <alexanderdauncey@gmail.com>
2021-05-31 00:33:29 +0100

    finish armor capability
    armor set takes from the proper files

--- src/main/java/net/shylex/semod/setup/ModArmorMaterial.java
+++ src/main/java/net/shylex/semod/setup/ModArmorMaterial.java
@@ -7,11 +7,14 @@ import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.util.LazyValue;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.util.SoundEvents;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.shylex.semod.SeMod;
 
 import java.util.function.Supplier;
 
 public enum ModArmorMaterial implements IArmorMaterial {
-    CUM(":cum", 15, new int[]{2, 5, 6, 2}, 9, SoundEvents.ARMOR_EQUIP_IRON, 0.0F, 0.0F, () -> {
+    CUM(SeMod.MOD_ID+":cum", 15, new int[]{2, 5, 6, 2}, 9, SoundEvents.ARMOR_EQUIP_IRON, 0.0F, 0.0F, () -> {
         return Ingredient.of(ModItems.CUM_JELLY.get());});
 
 
@@ -26,8 +29,8 @@ public enum ModArmorMaterial implements IArmorMaterial {
     private final float knockbackResistance;
     private final Supplier<Ingredient> repairIngredient;
 
-    private ModArmorMaterial(String name, int durabilityMultiplier, int[] slot, int enchantmentValue, SoundEvent soundtype, float toughness, float p_i231593_9_, Supplier<Ingredient> repairIngredient) {
-        this.name = name;
+    private ModArmorMaterial(String nameIn, int durabilityMultiplier, int[] slot, int enchantmentValue, SoundEvent soundtype, float toughness, float p_i231593_9_, Supplier<Ingredient> repairIngredient) {
+        this.name = nameIn;
         this.durabilityMultiplier = durabilityMultiplier;
         this.slotProtections = slot;
         this.enchantmentValue = enchantmentValue;
@@ -64,6 +67,7 @@ public enum ModArmorMaterial implements IArmorMaterial {
         return this.repairIngredient.get();
     }
 
+    @OnlyIn(Dist.CLIENT)
     @Override
     public String getName() {
         return this.name;
